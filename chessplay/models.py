feb7f84a4c2c8eb0ee3769232a909ce595e9f332
Lali Bibilashvili <lalibibilashvili@gmail.com>
2023-09-28 19:02:05 +0200

    game and player models created

--- chessplay/models.py
+++ chessplay/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from registration.models import Player
 
-# Create your models here.
+class Game(models.Model):
+
+    white_player_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='white_player')
+    black_player_id = models.ForeignKey(Player, on_delete=models.CASCADE, related_name='black_player')
+    moves = models.TextField() # PGN format
+
+    result = models.CharField(max_length=10, choices=["1:0", "0:1", "1/2-1/2", "-:-", "+:-", "-:+"], default="-:-")
+    status = models.CharField(max_length=10, choices=["not started", "ongoing", "finished"], default='not started')
+    date_played = models.DateTimeField(auto_now_add=True)
+    winner = models.IntegerField()
+
+    def __str__(self):
+        return f"Game between {self.white_player.user.username} and {self.black_player.user.username}, result: "
+
+    def set_result(self, game_result):
+        self.result = game_result
+
+        if game_result == "-:-":
+            return "not result"
+
+        self.white_player.game_played += 1
+        self.black_player.game_played += 1
+
+        if game_result == "1:0" or game_result == "+:-":
+            self.white_player.game_won += 1
+            self.black_player.game_lost += 1
+        elif game_result == "0:1" or game_result == "-:+":
+            self.white_player.game_lost += 1
+            self.black_player.game_won += 1
+        elif game_result == "1/2-1/2":
+            self.white_player.game_draw += 1
+            self.black_player.game_draw += 1
+        else:
+            self.white_player.game_lost += 0
+            self.black_player.game_lost += 0
+
+
+        self.white_player.save()
+        self.black_player.save()
+        self.save()
--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Player(models.Model):
+    player = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=1000)
+    games_played = models.IntegerField(default=0)
+    games_won = models.IntegerField(default=0)
+    games_lost = models.IntegerField(default=0)
+    games_draw = models.IntegerField(default=0)
+    opponents = models.ManyToManyField('self', through="Game",symmetrical=True)
+
+    def __str__(self):
+        self.user.username
